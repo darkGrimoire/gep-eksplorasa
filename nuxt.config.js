20f7f99412f40a99b567884ae372a303b9cdce82
Faris Rizki Ekananda <13518125@std.stei.itb.ac.id>
2020-12-03 15:20:15 +0700

    adding fonts &  eslint config cleaning stuffs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,10 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   mode: 'universal',
   target: 'server',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - gep-eksplorasa',
-    title: 'gep-eksplorasa',
+    titleTemplate: '%s - GEP',
+    title: 'GEP Eksplorasa',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -19,6 +17,8 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    "@/assets/scss/main.scss",
+    "@/assets/scss/fonts.scss",
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
@@ -63,21 +63,6 @@ export default {
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
   },
 
   generate: {
