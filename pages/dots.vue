4e35431fae606c330b79dc46070b7285be689429
regnents <rafaelseanputra@gmail.com>
2021-01-05 14:07:53 +0700

    try styling frontend

--- pages/dots.vue
+++ pages/dots.vue
@@ -3,9 +3,10 @@
     <div class="title">
       <h3>CONNECT THE DOTS</h3>
     </div>
-    <canvas id="test_canvas" @mousedown="createDoodle" />
-    <div class="dots_teks">
+    <div class="dots_board">
+      <canvas id="test_canvas" @mousedown="createDoodle" />
     </div>
+    <div class="dots_teks"></div>
     <div class="footer">
       <button id="back_button">Back</button>
       <button id="skip_button" @click="skipCanvas()">Skip</button>
@@ -19,16 +20,13 @@ export default {
   name: "Dots",
   data() {
     return {
-      coord: [
-        [10, 10],
-        [200, 10],
-        [200, 100],
-        [50, 100]
-      ],
+      coord: [],
       pos: 0
     }
   },
-  beforeMount() {
+  mounted() {
+    this.createCanvas()
+    this.createCoordinate()
     this.createPoint()
   },
   methods: {
@@ -48,20 +46,43 @@ export default {
       const y = event.clientY - rect.top
       return [x, y]
     },
+    createCanvas() {
+      let c = document.getElementById("test_canvas")
+      let document_height = 1.90* document.getElementsByClassName("connect_dots_page")[0].scrollHeight
+      let document_width = document.getElementsByClassName("dots_board")[0].scrollWidth
+      c.width = document_width
+      c.height = document_height
+    },
+    createCoordinate() {
+      this.coord.push([500, 500],
+        [540, 240],
+        [340, 220],
+        [700, 20],
+        [1100, 220],
+        [860,240],
+        [900, 500])
+    },
     createPoint() {
       let c = document.getElementById("test_canvas")
       let ctx = c.getContext("2d")
+      console.log(this.coord.length)
       for (let i = 0; i < this.coord.length; i++) {
+        ctx.fillStyle = "#000000"
         ctx.fillRect(this.coord[i][0], this.coord[i][1], 5, 5)
+        ctx.font = "25px Mechanical Pencil"
+        ctx.fillText(i+1, this.coord[i][0] - 15, this.coord[i][1] + 7)
       }
     },
     createLine(coord_awal, coord_akhir) {
       let c = document.getElementById("test_canvas")
       let ctx = c.getContext("2d")
+      // const rc = VueRough.rough.canvas(c)
+      // rc.line(coord_awal[0], coord_awal[1],coord_akhir[0], coord_akhir[1])
       ctx.beginPath()
       ctx.moveTo(coord_awal[0], coord_awal[1])
       ctx.lineTo(coord_akhir[0], coord_akhir[1])
       ctx.strokeStyle = "black"
+      ctx.lineWidth = 3
       ctx.stroke()
     },
     removeSkip() {
@@ -95,20 +116,28 @@ export default {
 
 <style lang="scss" scoped>
 .connect_dots_page {
-    font-family: 'Mechanical Pencil';
-    font-size: 40px;
+  font-family: "Mechanical Pencil";
+  font-size: 40px;
 }
 .title {
   text-align: center;
   margin: 10px 0px 15px 0px;
 }
+.dots_board {
+  width: calc(100% - 30px);
+  margin: 0px 15px 0px 15px;
+}
 canvas {
   border: 3px solid black;
+  background-color: yellow;
 }
 button {
+  border: solid 5px #41403e;
+  transition: 0.5s;
   background: transparent;
   padding: 0.5rem 0.5rem;
   margin: 0 0.5rem;
+  font-family: "Mechanical Pencil";
   font-size: 1rem;
 
   border-top-left-radius: 15px 50px;
@@ -116,11 +145,7 @@ button {
   border-bottom-right-radius: 15px 50px;
   border-bottom-left-radius: 50px 15px;
 }
-#next_button {
-  border: solid 5px #41403e;
-  transition: 0.5s;
-}
-#next_button:hover {
+button:hover {
   background-color: #41403e;
   color: whitesmoke;
 }
