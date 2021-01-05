ea238880cfb85c25f73cf8053222fb3c7bd9d0b3
poehlerity <calistahenlyyy@gmail.com>
2021-01-05 18:02:53 +0700

    beres layout + rotate maskot

--- pages/depan.vue
+++ pages/depan.vue
@@ -1,33 +1,40 @@
 <template>
-  <div class="outer-container">
-    <div class="slideshow">
-      <div class="logo">
-        <img src="/logo/EKSPLO2_UTAMA.png" style="width:100%">
-      </div>
+<div class="container">
+    <div class="content">
+        <div class="atas">
+            <div class="fear">
+                <img src="/maskot/emo-fear.png" style="width:100%">
+            </div>
 
-      <div class="logo"> 
-        <img src="/logo/EKSPLO2_ijo.png" style="width:100%">
-      </div>
+            <div class="sad">
+                <img src="/maskot/emo-sad.png" style="width:100%">
+            </div>
+        </div>
 
-      <div class="logo">
-        <img src="/logo/EKSPLO2_kuning.png" style="width:100%">
-      </div>
+        <div class="slideshow">
+            <!-- placeholder -->
+            <div class="logo"> 
+                <img src="/logo/EKSPLO2_kuning.png" style="width:100%">
+            </div>
+        </div>
 
-      <div class="logo">
-        <img src="/logo/EKSPLO2_merah.png" style="width:100%">
-      </div>
+        <div class="start">
+            <button type="button">
+                start explore!
+            </button>
+        </div>
 
-      <div class="logo">
-        <img src="/logo/EKSPLO2_biru.png" style="width:100%">
-      </div>
+        <div class="bawah">
+            <div class="joy">
+                <img src="/maskot/emo-joy.png" style="width:100%">
+            </div>
+
+            <div class="anger">
+                <img src="/maskot/emo-anger.png" style="width:100%">
+            </div>
+        </div>
     </div>
-
-    <div class="start">
-      <button type="button">
-        start explore!
-      </button>
-    </div>
-  </div>
+</div>
 </template>
 
 <script>
@@ -37,34 +44,100 @@
 </script>
 
 <style lang="scss" scoped>
-* {box-sizing: border-box;}
-img {vertical-align: middle;}
+    * {
+        box-sizing: border-box;
+    }
+
+    .container {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -moz-transform: translateX(-50%) translateY(-50%);
+        -webkit-transform: translateX(-50%) translateY(-50%);
+        transform: translateX(-50%) translateY(-50%);
+        text-align: center;
+    }
+    
+    img {
+        vertical-align: middle;
+    }
+
+    .fear {
+        max-width: 150px;
+        position: relative;
+        margin-right: 175px;
+        transform: rotate(45deg);
+        transition: transform 1s;
+    }
+
+    .fear:hover {
+        transform: rotate(20deg);
+    }
+
+    .sad {
+        max-width: 150px;
+        position: relative;
+        margin-left: 175px;
+        transition: transform 1s;
+    }
+
+    .sad:hover {
+        transform: rotate(25deg);
+    }
+
+    .joy {
+        max-width: 150px;
+        position: relative;
+        margin-right: 300px;
+        transform: rotate(20deg);
+        transition: transform 1s;
+    }
+
+    .joy:hover {
+        transform: rotate(-20deg);
+    }
+
+    .anger {
+        max-width: 150px;
+        position: relative;
+        margin-left: 300px;
+        transform: rotate(-20deg);
+        transition: transform 1s;
+    }
+
+    .anger:hover {
+        transform: rotate(20deg);
+    }
+
+    .atas {
+        display: flex;
+        justify-content: center;
+        padding: 50px;
+    }
+
+    .bawah {
+        display: flex;
+        justify-content: center;
+        padding: 25px;
+    }
 
     /* Slideshow container */
     .slideshow {
-    max-width: 500px;
+    max-width: 1000px;
     position: relative;
     margin: auto;
     }
 
-.outer-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-}
+    .start {
+        align-content: center;
+        vertical-align: middle;
+        padding: 20px;
+    }
 
-.start {
-    align-content: center;
-    vertical-align: middle;
-    padding: 20px;
-}
-
-button {
-    font-family: Mechanical Pencil;
-    font-size: 30px;
-    align-content: center;
-    vertical-align: middle;
-}
-
+    button {
+        font-family: Mechanical Pencil;
+        font-size: 50px;
+        align-content: center;
+        vertical-align: middle;
+    }
 </style>
