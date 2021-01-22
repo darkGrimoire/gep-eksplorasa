b72750cf0ca58cf095e06149cf8f49fda8ae136d
regnents <rafaelseanputra@gmail.com>
2021-01-22 19:42:05 +0700

    Update layout before renovation

--- pages/dots.vue
+++ pages/dots.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="connect_dots_page">
-    <div class="title">
+  <div class="connect-dots_page">
+    <div class="judul">
       <h3>CONNECT THE DOTS</h3>
     </div>
-    <div class="dots_board">
-      <canvas id="test_canvas" @mousedown="createDoodle" />
+    <div class="main_content">
+      <div class="pic">
+        <img src="/3-2.png" class="sky_pic" />
+        <img src="/3-1.png" class="dots_pic" />
+      </div>
+      <div class="dots_teks">DELAILAHIA</div>
+      <div class="dots_board">
+        <canvas id="dots_canvas" @mousemove="handleMouseMove" />
+      </div>
     </div>
-    <div class="dots_teks"></div>
     <div class="footer">
       <button id="back_button">Back</button>
       <button id="skip_button" @click="skipCanvas()">Skip</button>
@@ -21,24 +27,75 @@ export default {
   data() {
     return {
       coord: [],
-      pos: 0
+      pos: 0,
+      mouse_in_pos: false
     }
   },
   mounted() {
-    this.createCanvas()
-    this.createCoordinate()
-    this.createPoint()
+    this.setUpPic()
+    this.setUpCanvas()
+    this.addCoordinate()
+    window.addEventListener("resize",this.windowChange)
+  },
+  destroyed() {
+    window.removeEventListener("resize",this.windowChange)
   },
   methods: {
-    isPointInRange(point, coord_range) {
-      for (let i = 0; i < 2; i++) {
-        console.log("Titik harusnya: " + coord_range[i])
-        console.log("Titik yang diklik: " + point[i])
-        if ((coord_range[i] - 7 > point[i]) | (coord_range[i] + 7 < point[i])) {
-          return false
-        }
+    windowChange() {
+      this.setUpCanvas()
+      this.addCoordinate()
+      this.retrieveCreatedLine()
+    },
+    setUpPic() {
+      console.log(document.body.clientHeight)
+      document.getElementsByClassName("sky_pic")[0].style.height = document.body.clientHeight
+      document.getElementsByClassName("sky_pic")[0].style.visibility = "visible"
+    },
+    setUpCanvas() {
+      let c = document.getElementById("dots_canvas")
+      // c.width = document.getElementsByClassName("pic")[0].offsetWidth
+      c.width = document.body.clientWidth
+      c.height = document.getElementsByClassName("pic")[0].offsetHeight
+    },
+    addCoordinate() {
+      this.coord = []
+      const c = document.getElementById("dots_canvas")
+      const skyPicHeight = document.getElementsByClassName("sky_pic")[0]
+        .offsetHeight
+      const dotPicHeight = document.getElementsByClassName("dots_pic")[0]
+        .offsetHeight
+      const dotPicWidth = document.getElementsByClassName("dots_pic")[0]
+        .offsetWidth
+      console.log(dotPicWidth)
+      console.log(c.width)
+      let coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 25.3) / 100
+      let coord_y = skyPicHeight + (dotPicHeight * 84.7) / 100
+      this.coord.push([coord_x, coord_y])
+      coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 26.5) / 100
+      coord_y = skyPicHeight + (dotPicHeight * 43.9) / 100
+      this.coord.push([coord_x, coord_y])
+      coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 13) / 100
+      coord_y = skyPicHeight + (dotPicHeight * 43.4) / 100
+      this.coord.push([coord_x, coord_y])
+      coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 52) / 100
+      coord_y = skyPicHeight + (dotPicHeight * 7.5) / 100
+      this.coord.push([coord_x, coord_y])
+      coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 90) / 100
+      coord_y = skyPicHeight + (dotPicHeight * 43.4) / 100
+      this.coord.push([coord_x, coord_y])
+      coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 74.7) / 100
+      coord_y = skyPicHeight + (dotPicHeight * 43.9) / 100
+      this.coord.push([coord_x, coord_y])
+      coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 74.9) / 100
+      coord_y = skyPicHeight + (dotPicHeight * 85.8) / 100
+      this.coord.push([coord_x, coord_y])
+      // DEBUGGING SECTION
+      let ctx = c.getContext("2d")
+      for (let i = 0; i < this.coord.length; i ++) {
+        ctx.fillStyle = "yellow"
+        ctx.fillRect(this.coord[i][0],this.coord[i][1],5,5)
       }
-      return true
+      // END OF DEBUGGING SECTION
     },
     getCursorPosition(canvas, event) {
       const rect = canvas.getBoundingClientRect()
@@ -46,38 +103,20 @@ export default {
       const y = event.clientY - rect.top
       return [x, y]
     },
-    createCanvas() {
-      let c = document.getElementById("test_canvas")
-      let document_height = 1.90* document.getElementsByClassName("connect_dots_page")[0].scrollHeight
-      let document_width = document.getElementsByClassName("dots_board")[0].scrollWidth
-      c.width = document_width
-      c.height = document_height
-    },
-    createCoordinate() {
-      this.coord.push([500, 500],
-        [540, 240],
-        [340, 220],
-        [700, 20],
-        [1100, 220],
-        [860,240],
-        [900, 500])
-    },
-    createPoint() {
-      let c = document.getElementById("test_canvas")
-      let ctx = c.getContext("2d")
-      console.log(this.coord.length)
-      for (let i = 0; i < this.coord.length; i++) {
-        ctx.fillStyle = "#000000"
-        ctx.fillRect(this.coord[i][0], this.coord[i][1], 5, 5)
-        ctx.font = "25px Mechanical Pencil"
-        ctx.fillText(i+1, this.coord[i][0] - 15, this.coord[i][1] + 7)
+    isPointInRange(point, coord_range) {
+      let batasRange = []
+      batasRange.push((document.getElementsByClassName("dots_pic")[0].offsetWidth * 1.61) / 100)
+      batasRange.push((document.getElementsByClassName("dots_pic")[0].offsetHeight * 1.62) / 100)
+      for (let i = 0; i < 2; i++) {
+        if ((coord_range[i] - batasRange[i] > point[i]) | (coord_range[i] + batasRange[i] < point[i])) {
+          return false
+        }
       }
+      return true
     },
     createLine(coord_awal, coord_akhir) {
-      let c = document.getElementById("test_canvas")
+      let c = document.getElementById("dots_canvas")
       let ctx = c.getContext("2d")
-      // const rc = VueRough.rough.canvas(c)
-      // rc.line(coord_awal[0], coord_awal[1],coord_akhir[0], coord_akhir[1])
       ctx.beginPath()
       ctx.moveTo(coord_awal[0], coord_awal[1])
       ctx.lineTo(coord_akhir[0], coord_akhir[1])
@@ -95,41 +134,94 @@ export default {
       }
       this.removeSkip()
     },
-    createDoodle: function(event) {
-      if (this.pos >= this.coord.length - 1) {
+    handleMouseMove(e) {
+      if (this.pos == this.coord.length - 1) {
         return
       }
-      let c = document.getElementById("test_canvas")
-      let x = this.getCursorPosition(c, event)
-      let is_in_position = this.isPointInRange(x, this.coord[this.pos + 1])
-      if (is_in_position) {
-        this.createLine(this.coord[this.pos], this.coord[this.pos + 1])
-        this.pos = this.pos + 1
-        if (this.pos == this.coord.length - 1) {
-          this.removeSkip()
+      let c = document.getElementById("dots_canvas")
+      const mouse_coord = this.getCursorPosition(c, e)
+      if (this.isPointInRange(mouse_coord, this.coord[this.pos])) {
+        this.mouse_in_pos = true
+      }
+      if (this.mouse_in_pos) {
+        this.clearCanvas()
+        this.retrieveCreatedLine()
+        this.createLine(this.coord[this.pos], mouse_coord)
+        if (this.isPointInRange(mouse_coord, this.coord[this.pos + 1])) {
+          this.pos = this.pos + 1
+          this.showTeks(this.pos)
+          if (this.pos == this.coord.length - 1) {
+            this.mouse_in_pos = false
+          }
         }
       }
+    },
+    clearCanvas() {
+      let c = document.getElementById("dots_canvas")
+      let ctx = c.getContext("2d")
+      ctx.clearRect(0, 0, c.width, c.height)
+      
+    },
+    retrieveCreatedLine() {
+      let i = 0
+      while (i < this.pos) {
+        this.createLine(this.coord[i], this.coord[i + 1])
+        i = i + 1
+      }
+    },
+    showTeks(urutan) {
+      // Belum SELESAI
+      console.log(urutan)
+      document.getElementsByClassName("dots_teks")[0].style.visibility =
+        "visible"
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.connect_dots_page {
+.connect-dots_page {
+  background-image: url("/bg_home.jpg");
+}
+.judul {
   font-family: "Mechanical Pencil";
   font-size: 40px;
-}
-.title {
   text-align: center;
-  margin: 10px 0px 15px 0px;
+  padding: 10px 0px 0px 0px;
+}
+.main_content {
+  position: relative;
+}
+.pic {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.sky_pic {
+  visibility: hidden;
+  // height: 9vw;
+  z-index: 1;
+  // max-height: 15vh;
+}
+.dots_pic {
+  height: 30vw;
+  z-index: 0;
 }
 .dots_board {
-  width: calc(100% - 30px);
-  margin: 0px 15px 0px 15px;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  z-index: 1;
 }
-canvas {
-  border: 3px solid black;
-  background-color: yellow;
+// canvas {
+//   background-color: yellow;
+// }
+.dots_teks {
+  // visibility: hidden;
+  font-family: "Mechanical Pencil";
+  font-size: 40px;
+  text-align: center;
 }
 button {
   border: solid 5px #41403e;
