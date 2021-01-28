e4c14ad95109130475d8df5c339090b82e6b736e
raf555 <irsoekarno888@gmail.com>
2021-01-28 21:03:19 +0700

    show turnjs when fullscreen

---
+++ pages/testurn.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="container" :style="[ fs ? { backgroundImage: `url(/img/uc-bg.png)` } : null ]">
+    <!--<pre>  Full screen : {{ fs }}
+    <button @click="fulskrin()">Go {{ fs?"back":"fullscreen" }}</button>
+    </pre>-->
+    <button @click="turnPrevious" v-show="fs">
+      Previous
+    </button>
+    <fw-turn id="album" ref="book" :options="option" v-show="fs">
+      <div v-for="i in 6" :key="i">
+        <img :src="`/book/${i}.jpg`">
+      </div>
+    </fw-turn>
+    <button @click="turnNext" v-show="fs">
+      Next
+    </button>
+  </div>
+</template>
+
+<script>
+import $ from "jquery"
+
+  export default {
+    name: "TestTurn",
+    data() {
+      return {
+        fs: false,
+        option: {
+          width: 500*2,
+          height: 500/(1076/1521),
+          autoCenter: true,
+          display: "single"
+        },
+        selector: undefined
+      }
+    },
+    beforeMount (){
+        document.addEventListener('keydown', this.keyfs)
+        document.addEventListener("fullscreenchange", this.changefs)
+        document.addEventListener("mozfullscreenchange", this.changefs)
+        document.addEventListener("webkitfullscreenchange", this.changefs)
+        document.addEventListener("msfullscreenchange", this.changefs)
+    },
+    beforeDestroy() {
+        document.removeEventListener('keydown', this.keyfs)
+        document.removeEventListener("fullscreenchange", this.changefs)
+        document.removeEventListener("mozfullscreenchange", this.changefs)
+        document.removeEventListener("webkitfullscreenchange", this.changefs)
+        document.removeEventListener("msfullscreenchange", this.changefs)
+    },
+    mounted () {
+      this.selector = this.$refs.book.selector
+    },
+    methods: {
+        keyfs (e) {
+            if(e.code=="F11"){
+                e.preventDefault()
+                this.fulskrin()
+            }
+        },
+        changefs () {
+            this.fs = !this.fs
+        },
+        turnNext() {
+            $(this.selector).turn('next')
+        },
+        turnPrevious() {
+            $(this.selector).turn('previous')
+        },
+        fulskrin () {
+                let elem = document.documentElement
+                if(!this.fs) {
+                    //$("#tes").attr("style", "background-image: url(\"/img/uc-bg.png\");")
+                    if (elem.requestFullscreen) {
+                        elem.requestFullscreen()
+                    } else if (elem.mozRequestFullScreen) { /* Firefox */
+                        elem.mozRequestFullScreen()
+                    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+                        elem.webkitRequestFullscreen()
+                    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+                        elem = window.top.document.body //To break out of frame in IE
+                        elem.msRequestFullscreen()
+                    }
+                } else {
+                      if (document.exitFullscreen) {
+                            document.exitFullscreen()
+                        } else if (document.mozCancelFullScreen) {
+                            document.mozCancelFullScreen()
+                        } else if (document.webkitExitFullscreen) {
+                            document.webkitExitFullscreen()
+                        } else if (document.msExitFullscreen) {
+                            window.top.document.msExitFullscreen()
+                        }
+                }
+        }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    overflow: hidden;
+    /*background-image: url("/img/uc-bg.png");*/
+    background-size: cover;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #album {
+    //margin: 5%;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+</style>
