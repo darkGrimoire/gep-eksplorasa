b7b34f31941a39bf314a6780773ffeea532af24a
Faris Rizki Ekananda <13518125@std.stei.itb.ac.id>
2021-01-28 13:26:02 +0700

    disable redirect for dev

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
 
   router: {
     // ran before every route on both client and server
-    middleware: 'redirect'
+    // middleware: 'redirect' // ONLY ENABLE IN MASTER BRANCH COMING SOON DEPLOYMENT
   },
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
