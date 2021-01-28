7409b5cb3fb347868a4e6267cdb3a92da93081da
Faris Rizki Ekananda <13518125@std.stei.itb.ac.id>
2021-01-28 16:26:32 +0700

    turnjs testing

---
+++ pages/karya/buku.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <fw-turn id="album" :options="option">
+      <div v-for="i in 6" :key="i">
+        <img :src="`/book/${i}.jpg`">
+      </div>
+    </fw-turn>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TestBuku",
+    data() {
+      return {
+        option: {
+          width: 500*2,
+          height: 500/(1076/1521),
+          autoCenter: true,
+        }
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    overflow: hidden;
+    background-image: url("/img/uc-bg.png");
+    background-size: cover;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #album {
+    margin: 5%;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+</style>
---
+++ plugins/vue-turnjs.client.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import { FlippingWidgets } from "vue-turnjs"
+import "vue-turnjs/dist/vue-turnjs.esm.css"
+
+Vue.use(FlippingWidgets)
