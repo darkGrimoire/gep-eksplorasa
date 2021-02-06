b009c3f1cf9de61ea91c4f45337e402abba22cff
regnents <rafaelseanputra@gmail.com>
2021-02-06 14:06:04 +0700

    major change to layout

--- pages/dots.vue
+++ pages/dots.vue
@@ -1,22 +1,29 @@
 <template>
-  <div class="connect-dots_page">
+  <div class="connect-dots-page">
     <div class="judul">
-      <h3>CONNECT THE DOTS</h3>
+      <img src="/title.png" class="img-judul" />
     </div>
-    <div class="main_content">
-      <div class="pic">
-        <img src="/3-2.png" class="sky_pic" />
-        <img src="/3-1.png" class="dots_pic" />
-      </div>
-      <div class="dots_teks">DELAILAHIA</div>
-      <div class="dots_board">
-        <canvas id="dots_canvas" @mousemove="handleMouseMove" />
-      </div>
+    <div class="caption">
+      <img src="/caption-1.png" class="caption-item cap-1" />
+      <img src="/caption-2.png" class="caption-item cap-2" />
+      <img src="/caption-3.png" class="caption-item cap-3" />
+      <img src="/caption-4.png" class="caption-item cap-4" />
+      <img src="/caption-5.png" class="caption-item-2 cap-5" />
+      <img src="/caption-6.png" class="caption-item-2 cap-6" />
+      <img src="/caption-7.png" class="caption-item-3 cap-6" />
     </div>
-    <div class="footer">
-      <button id="back_button">Back</button>
-      <button id="skip_button" @click="skipCanvas()">Skip</button>
-      <button id="next_button">Next</button>
+    <div class="dots">
+      <img src="/dots_1.png" class="dots-1" />
+      <img src="/dots_2.png" class="dots-2" />
+      <img src="/home-bgbest.gif" class="dots-gif" />
+      <img src="/dots_3_grass.png" class="dots-3" />
+      <img src="/dots_3_flower.png" class="dots-4" />
+      <img src="/dots_3_flower.png" class="dots-5" />
+      <canvas
+        class="dots-canvas"
+        id="dots_canvas"
+        @mousemove="handleMouseMove"
+      />
     </div>
   </div>
 </template>
@@ -28,6 +35,7 @@ export default {
     return {
       coord: [],
       pos: 0,
+      additional_grass: 0,
       mouse_in_pos: false
     }
   },
@@ -35,66 +43,115 @@ export default {
     this.setUpPic()
     this.setUpCanvas()
     this.addCoordinate()
-    window.addEventListener("resize",this.windowChange)
+    window.addEventListener("resize", this.windowChange)
   },
   destroyed() {
-    window.removeEventListener("resize",this.windowChange)
+    window.removeEventListener("resize", this.windowChange)
   },
   methods: {
     windowChange() {
+      this.setUpPic()
       this.setUpCanvas()
       this.addCoordinate()
       this.retrieveCreatedLine()
     },
     setUpPic() {
-      console.log(document.body.clientHeight)
-      document.getElementsByClassName("sky_pic")[0].style.height = document.body.clientHeight
-      document.getElementsByClassName("sky_pic")[0].style.visibility = "visible"
+      let dots3_top = 0
+      let dots2_top = 0
+      const dotsPic1 = document.getElementsByClassName("dots-1")[0]
+      const dotsPic2 = document.getElementsByClassName("dots-2")[0]
+      const dotsPic3 = document.getElementsByClassName("dots-3")[0]
+      const dotsPic4 = document.getElementsByClassName("dots-4")[0]
+      const dotsPic5 = document.getElementsByClassName("dots-5")[0]
+      const dotsGif = document.getElementsByClassName("dots-gif")[0]
+      if (window.innerWidth < 1024) {
+        dots2_top = dotsPic1.clientHeight
+        dots3_top = dotsPic1.clientHeight + dotsPic2.clientHeight * 0.5
+      } else {
+        const windowHeight = window.innerHeight
+        const dots3_height = dotsPic3.offsetHeight
+        const dotsHeight =
+          windowHeight -
+          document.getElementsByClassName("judul")[0].clientHeight -
+          document.getElementsByClassName("caption")[0].clientHeight
+        dots3_top = dotsHeight - dots3_height
+        dots2_top = dots3_top + dots3_height * 0.8 - dotsPic2.offsetHeight
+      }
+      dotsGif.style.width = dotsPic2.style.width
+      const bunga_top = parseInt(dots3_top) + (dotsPic3.offsetHeight * 0.21)
+      const gif_top =
+        parseInt(dots2_top) +
+        dotsPic2.clientHeight -
+        dotsGif.clientHeight -
+        dotsPic2.clientHeight * 0.14
+      // SET THE TOP MARGIN OF ALL PIC AND GIF
+      dotsGif.style.top = gif_top + "px"
+      dotsPic2.style.top = dots2_top + "px"
+      dotsPic3.style.top = dots3_top + "px"
+      dotsPic4.style.top = bunga_top + "px"
+      dotsPic5.style.top = bunga_top + "px"
+      
+      dotsPic1.style.visibility = "visible"
+      dotsPic2.style.visibility = "visible"
+      dotsPic3.style.visibility = "visible"
+      dotsPic4.style.visibility = "visible"
+      dotsPic5.style.visibility = "visible"
+      
+      
+      
     },
     setUpCanvas() {
       let c = document.getElementById("dots_canvas")
-      // c.width = document.getElementsByClassName("pic")[0].offsetWidth
+      document.getElementsByClassName(
+        "dots-canvas"
+      )[0].style.top = document.getElementsByClassName("dots-2")[0].style.top
       c.width = document.body.clientWidth
-      c.height = document.getElementsByClassName("pic")[0].offsetHeight
+      c.height = document.getElementsByClassName("dots-2")[0].offsetHeight
     },
     addCoordinate() {
       this.coord = []
       const c = document.getElementById("dots_canvas")
-      const skyPicHeight = document.getElementsByClassName("sky_pic")[0]
-        .offsetHeight
-      const dotPicHeight = document.getElementsByClassName("dots_pic")[0]
-        .offsetHeight
-      const dotPicWidth = document.getElementsByClassName("dots_pic")[0]
+      const dots_pic_width = document.getElementsByClassName("dots-2")[0]
         .offsetWidth
-      console.log(dotPicWidth)
-      console.log(c.width)
-      let coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 25.3) / 100
-      let coord_y = skyPicHeight + (dotPicHeight * 84.7) / 100
+      const canvas_width = c.width
+      const dots_pic_height = c.height
+      const x_start = (canvas_width - dots_pic_width) / 2
+      let coord_x = 0
+      let coord_y = 0
+      // FIRST POINT
+      coord_x = x_start + dots_pic_width * 0.25
+      coord_y = dots_pic_height * 0.85
       this.coord.push([coord_x, coord_y])
-      coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 26.5) / 100
-      coord_y = skyPicHeight + (dotPicHeight * 43.9) / 100
+      // SECOND POINT
+      coord_x = x_start + dots_pic_width * 0.265
+      coord_y = dots_pic_height * 0.44
       this.coord.push([coord_x, coord_y])
-      coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 13) / 100
-      coord_y = skyPicHeight + (dotPicHeight * 43.4) / 100
+      // THIRD POINT
+      coord_x = x_start + dots_pic_width * 0.13
+      coord_y = dots_pic_height * 0.435
       this.coord.push([coord_x, coord_y])
-      coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 52) / 100
-      coord_y = skyPicHeight + (dotPicHeight * 7.5) / 100
+      // FOURTH POINT
+      coord_x = x_start + dots_pic_width * 0.52
+      coord_y = dots_pic_height * 0.075
       this.coord.push([coord_x, coord_y])
-      coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 90) / 100
-      coord_y = skyPicHeight + (dotPicHeight * 43.4) / 100
+      // FIFTH POINT
+      coord_x = x_start + dots_pic_width * 0.9
+      coord_y = dots_pic_height * 0.435
       this.coord.push([coord_x, coord_y])
-      coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 74.7) / 100
-      coord_y = skyPicHeight + (dotPicHeight * 43.9) / 100
+      // SIXTH POINT
+      coord_x = x_start + dots_pic_width * 0.747
+      coord_y = dots_pic_height * 0.442
       this.coord.push([coord_x, coord_y])
-      coord_x = (c.width - dotPicWidth) / 2 + (dotPicWidth * 74.9) / 100
-      coord_y = skyPicHeight + (dotPicHeight * 85.8) / 100
+      // SEVENTH POINT
+      coord_x = x_start + dots_pic_width * 0.75
+      coord_y = dots_pic_height * 0.86
       this.coord.push([coord_x, coord_y])
       // DEBUGGING SECTION
-      let ctx = c.getContext("2d")
-      for (let i = 0; i < this.coord.length; i ++) {
-        ctx.fillStyle = "yellow"
-        ctx.fillRect(this.coord[i][0],this.coord[i][1],5,5)
-      }
+      // let ctx = c.getContext("2d")
+      // for (let i = 0; i < this.coord.length; i ++) {
+      //   ctx.fillStyle = "yellow"
+      //   ctx.fillRect(this.coord[i][0],this.coord[i][1],30,30)
+      // }
       // END OF DEBUGGING SECTION
     },
     getCursorPosition(canvas, event) {
@@ -105,10 +162,17 @@ export default {
     },
     isPointInRange(point, coord_range) {
       let batasRange = []
-      batasRange.push((document.getElementsByClassName("dots_pic")[0].offsetWidth * 1.61) / 100)
-      batasRange.push((document.getElementsByClassName("dots_pic")[0].offsetHeight * 1.62) / 100)
+      batasRange.push(
+        (document.getElementsByClassName("dots-2")[0].offsetWidth * 1.61) / 100
+      )
+      batasRange.push(
+        (document.getElementsByClassName("dots-2")[0].offsetHeight * 1.62) / 100
+      )
       for (let i = 0; i < 2; i++) {
-        if ((coord_range[i] - batasRange[i] > point[i]) | (coord_range[i] + batasRange[i] < point[i])) {
+        if (
+          (coord_range[i] - batasRange[i] > point[i]) |
+          (coord_range[i] + batasRange[i] < point[i])
+        ) {
           return false
         }
       }
@@ -124,16 +188,16 @@ export default {
       ctx.lineWidth = 3
       ctx.stroke()
     },
-    removeSkip() {
-      document.getElementById("skip_button").style.visibility = "hidden"
-    },
-    skipCanvas() {
-      while (this.pos < this.coord.length - 1) {
-        this.createLine(this.coord[this.pos], this.coord[this.pos + 1])
-        this.pos = this.pos + 1
-      }
-      this.removeSkip()
-    },
+    // removeSkip() {
+    //   document.getElementById("skip_button").style.visibility = "hidden"
+    // },
+    // skipCanvas() {
+    //   while (this.pos < this.coord.length - 1) {
+    //     this.createLine(this.coord[this.pos], this.coord[this.pos + 1])
+    //     this.pos = this.pos + 1
+    //   }
+    //   this.removeSkip()
+    // },
     handleMouseMove(e) {
       if (this.pos == this.coord.length - 1) {
         return
@@ -152,6 +216,8 @@ export default {
           this.showTeks(this.pos)
           if (this.pos == this.coord.length - 1) {
             this.mouse_in_pos = false
+            document.getElementsByClassName("dots-gif")[0].style.visibility =
+              "visible"
           }
         }
       }
@@ -160,7 +226,6 @@ export default {
       let c = document.getElementById("dots_canvas")
       let ctx = c.getContext("2d")
       ctx.clearRect(0, 0, c.width, c.height)
-      
     },
     retrieveCreatedLine() {
       let i = 0
@@ -170,75 +235,197 @@ export default {
       }
     },
     showTeks(urutan) {
-      // Belum SELESAI
-      console.log(urutan)
-      document.getElementsByClassName("dots_teks")[0].style.visibility =
-        "visible"
+      const cap_class = "cap-" + urutan
+      let cap_items = document.getElementsByClassName(cap_class)
+      for (const cap_item of cap_items) {
+        cap_item.style.visibility = "visible"
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.connect-dots_page {
+.connect-dots-page {
   background-image: url("/bg_home.jpg");
+  background-size: cover;
+  height: 100vh;
 }
+
 .judul {
-  font-family: "Mechanical Pencil";
-  font-size: 40px;
-  text-align: center;
-  padding: 10px 0px 0px 0px;
-}
-.main_content {
-  position: relative;
-}
-.pic {
+  padding-top: 0.5vw;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
+  flex-wrap: wrap;
   align-items: center;
 }
-.sky_pic {
+.img-judul {
+  width: 20%;
+}
+.caption {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+}
+.caption-item {
+  width: 10vw;
+}
+.caption-item-2 {
+  width: 6vw;
+}
+.caption-item-3 {
+  width: 3vw;
+}
+.cap-1,
+.cap-2,
+.cap-3,
+.cap-4,
+.cap-5,
+.cap-6 {
   visibility: hidden;
-  // height: 9vw;
-  z-index: 1;
-  // max-height: 15vh;
 }
-.dots_pic {
-  height: 30vw;
-  z-index: 0;
+.dots {
+  position: relative;
 }
-.dots_board {
+.dots-1 {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 15vw;
+  visibility: hidden;
+}
+.dots-2 {
+  width: 30vw;
   position: absolute;
   top: 0px;
-  left: 0px;
+  left: 50%;
+  transform: translateX(-50%);
+  visibility: hidden;
+  z-index: 3;
+}
+.dots-gif {
+  visibility: hidden;
+  width: 24vw;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 2;
+}
+.dots-3 {
+  position: absolute;
+  width: 100%;
+  top: 0px;
   z-index: 1;
+  visibility: hidden;
 }
-// canvas {
-//   background-color: yellow;
+.dots-4, .dots-5 {
+  position: absolute;
+  width: 30%;
+  z-index: 2;
+  visibility: hidden;
+}
+.dots-4 {
+  left: 7%;
+}
+.dots-5 {
+  left: 60%;
+}
+.dots-canvas {
+  position: absolute;
+  top: 10px;
+  left: 0px;
+  // background-color: yellow;
+  z-index: 4;
+}
+@media (max-width: 1024px) {
+  .img-judul {
+    width: 40%;
+  }
+  .caption-item {
+    width: 15vw;
+  }
+  .caption-item-2 {
+    width: 9vw;
+  }
+  .caption-item-3 {
+    width: 4.5vw;
+  }
+  .dots-1 {
+    height: 20vw;
+  }
+  .dots-2 {
+    width: 40vw;
+  }
+  .dots-gif {
+    width: 32vw;
+    top: 18vh;
+  }
+}
+// .dots-3 {
+//   width: 90vw;
+//   left: 50%;
+//   transform: translateX(-50%);
 // }
-.dots_teks {
-  // visibility: hidden;
-  font-family: "Mechanical Pencil";
-  font-size: 40px;
-  text-align: center;
-}
-button {
-  border: solid 5px #41403e;
-  transition: 0.5s;
-  background: transparent;
-  padding: 0.5rem 0.5rem;
-  margin: 0 0.5rem;
-  font-family: "Mechanical Pencil";
-  font-size: 1rem;
 
-  border-top-left-radius: 15px 50px;
-  border-top-right-radius: 50px 15px;
-  border-bottom-right-radius: 15px 50px;
-  border-bottom-left-radius: 50px 15px;
-}
-button:hover {
-  background-color: #41403e;
-  color: whitesmoke;
-}
+// .judul {
+//   font-family: "Mechanical Pencil";
+//   font-size: 40px;
+//   text-align: center;
+//   padding: 10px 0px 0px 0px;
+// }
+// .main_content {
+//   position: relative;
+// }
+// .pic {
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+// }
+// .sky_pic {
+//   visibility: hidden;
+//   // height: 9vw;
+//   z-index: 1;
+//   // max-height: 15vh;
+// }
+// .dots_pic {
+//   height: 30vw;
+//   z-index: 0;
+// }
+// .dots_board {
+//   position: absolute;
+//   top: 0px;
+//   left: 0px;
+//   z-index: 1;
+// }
+// // canvas {
+// //   background-color: yellow;
+// // }
+// .dots_teks {
+//   // visibility: hidden;
+//   font-family: "Mechanical Pencil";
+//   font-size: 40px;
+//   text-align: center;
+// }
+// button {
+//   border: solid 5px #41403e;
+//   transition: 0.5s;
+//   background: transparent;
+//   padding: 0.5rem 0.5rem;
+//   margin: 0 0.5rem;
+//   font-family: "Mechanical Pencil";
+//   font-size: 1rem;
+
+//   border-top-left-radius: 15px 50px;
+//   border-top-right-radius: 50px 15px;
+//   border-bottom-right-radius: 15px 50px;
+//   border-bottom-left-radius: 50px 15px;
+// }
+// button:hover {
+//   background-color: #41403e;
+//   color: whitesmoke;
+// }
 </style>
