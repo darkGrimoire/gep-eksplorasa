18581ed5833169b5f45538f0f64e22e1bea0e6ed
Faris Rizki Ekananda <13518125@std.stei.itb.ac.id>
2021-02-12 08:49:08 +0700

    ruangan joy done

---
+++ pages/ruangan/joy.vue
@@ -0,0 +1,383 @@
+<template>
+  <div class="main">
+    <div class="slide-controls">
+      <fa v-show="slide === 2" :icon="['fas', 'chevron-left']" class="left-arrow arrow" @click="switchSlide(-1)" />
+      <fa v-show="slide === 1" :icon="['fas', 'chevron-right']" class="right-arrow arrow" @click="switchSlide(1)" />
+    </div>
+    
+    <div id="slide0" class="top-cont" 
+         :style="{'transform': 'translate(calc('+base.slide0+'% '+sign+' '+Math.abs(computedDisplacement)+'px), -50%)'}" 
+    >
+      <div class="canvas">
+        <div class="canvas canvas-hover" />
+      </div>
+    </div>
+    
+    <!-- SLIDE 1 -->
+    <div id="slide1" class="top-cont" 
+         :style="{'transform': 'translate(calc('+base.slide1+'% '+sign+' '+Math.abs(computedDisplacement)+'px), -50%)'}" 
+         @mousedown="startDrag($event)" 
+         @mousemove="dragContainer($event)" 
+         @mouseup="endDrag($event)"
+         @mouseleave="endDrag($event)"
+         @touchstart="startDrag($event)" 
+         @touchmove="dragContainer($event)" 
+         @touchend="endDrag($event)"
+    >
+      <div class="canvas">
+        <div class="canvas canvas-hover">
+          <div class="cont guide">
+            <!-- Ubah src jadi guide image yang kamu inginkan, setel opacity sesuai keinginan. -->
+            <img src="/guide1.png" alt="guide" style="opacity: 0;">
+          </div>
+          <div class="cont sky">
+            <img src="/joy/sky 1.png" alt="sky">
+          </div>
+          <div class="cont pagar">
+            <img src="/joy/pager1 1.png" alt="pagar">
+          </div>
+          <div class="cont rumput">
+            <img src="/joy/rumput 1.png" alt="rumput">
+          </div>
+          <div class="cont bunga">
+            <img src="/joy/bunga 1.png" alt="bunga">
+          </div>
+          <div class="cont semak1">
+            <img src="/joy/semak1 1.png" alt="semak">
+          </div>
+          <div class="cont semak2">
+            <img src="/joy/semak2 1.png" alt="semak">
+          </div>
+          <div class="cont karpet">
+            <img src="/joy/karpet 1.png" alt="karpet">
+          </div>
+          <div class="cont tv">
+            <img src="/joy/tv 1.png" alt="tv">
+          </div>
+          <div class="cont pohon">
+            <img src="/joy/pohon 1.png" alt="pohon">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- SLIDE 2 -->
+    <div id="slide2" class="top-cont" 
+         :style="{'transform': 'translate(calc('+base.slide2+'% '+sign+' '+(Math.abs(computedDisplacement)-1)+'px), -50%)'}" 
+         @mousedown="startDrag($event)" 
+         @mousemove="dragContainer($event)" 
+         @mouseup="endDrag($event)"
+         @mouseleave="endDrag($event)"
+         @touchstart="startDrag($event)" 
+         @touchmove="dragContainer($event)" 
+         @touchend="endDrag($event)"
+    >
+      <div class="canvas">
+        <div class="canvas canvas-hover">
+          <div class="cont guide">
+            <!-- Ubah src jadi guide image yang kamu inginkan, setel opacity sesuai keinginan. -->
+            <img src="/guide2.png" alt="guide" style="opacity: 0;">
+          </div>
+          <div class="cont sky">
+            <img src="/joy/sky 1.png" alt="sky">
+          </div>
+          <div class="cont matahari">
+            <img src="/joy/sun 1.png" alt="matahari">
+          </div>
+          <div class="cont pagar">
+            <img src="/joy/pager2 1.png" alt="pagar">
+          </div>
+          <div class="cont rumput">
+            <img src="/joy/rumput 1.png" alt="rumput">
+          </div>
+          <div class="cont bunga2">
+            <img src="/joy/bunga2 1.png" alt="bunga">
+          </div>
+          <div class="cont tanaman">
+            <img src="/joy/taneman 1.png" alt="tanaman">
+          </div>
+          <div class="cont pasir">
+            <img src="/joy/pasir 1.png" alt="pasir">
+          </div>
+        </div>
+      </div>
+    </div>
+    <rcp />
+  </div>
+</template>
+
+<script>
+// --- keterangan tiap class
+// top-cont: tempat perhitungan rasio
+// canvas: kalau mau ada tooltip, taro disini
+// canvas-hover: kalau mau ada efek hover kaya di moooi, taro sini
+  const SCALE = 2
+  import gsap from 'gsap'
+  import rcp from '~/components/rcp.vue'
+  export default {
+    name: "TemplateRuangan",
+    components: {
+      rcp,
+    },
+    data() {
+      return {
+        drag: false,
+        mouseStart: {
+          x: undefined,
+          y: undefined // not used for now though
+        },
+        computedDisplacement: 0,
+        transformed: 0,
+        xBoundary: undefined,
+        slide: 0,
+        base: {
+          slide0: -50,
+          slide1: 50,
+          slide2: 150
+        },
+        msg: 'Pesan Kurator Here'
+      }
+    },
+    computed: {
+      sign() {
+        return this.computedDisplacement >= 0 ? '+' : '-'
+      }
+    },
+    watch: {
+      slide(newVal) {
+        if (newVal === 2){
+          gsap.to(this.base, {slide0: -250, slide1: -150, slide2: -50})
+        } else if (newVal === 1){
+          gsap.to(this.base, {slide0: -150, slide1: -50, slide2: 50})
+        } else if (newVal === 0){
+          gsap.to(this.base, {slide0: -50, slide1: 50, slide2: 150})
+        } else {
+          gsap.to(this.base, {duration: 3, ease: 'none' ,slide0: -350, slide1: -250, slide2: -150})
+        }
+      }
+    },
+    mounted () {
+      this.xBoundary = document.getElementsByClassName("top-cont")[0].clientWidth
+      window.addEventListener("resize", this.handleResize)
+
+      // TODO: Add on enter animation here
+      this.slide = 1
+    },
+    methods: {
+      switchSlide(val){
+        this.slide += val
+        this.computedDisplacement = 0
+        this.transformed = 0
+      },
+      startDrag(e) {
+        if (window.matchMedia("(orientation: portrait)").matches){
+          // enable dragging and keep mouseStart point
+          let interaction = this.determineInteraction(e)
+          this.drag = true
+          this.mouseStart.x = interaction.clientX
+          this.mouseStart.y = interaction.clientY
+        }
+      },
+      dragContainer(e){
+        // if dragging, calculate target position and tween to that position
+        if (this.drag){
+          let interaction = this.determineInteraction(e)
+          let targetDisplacement = this.clamp(interaction.clientX - this.mouseStart.x + this.transformed, this.xBoundary/(2*SCALE), -this.xBoundary/(2*SCALE))
+          gsap.to(this.$data, {computedDisplacement: targetDisplacement})
+        }
+      },
+      endDrag(e){
+        // if mouse click released or out of window while dragging, disable dragging,
+        // calculate target end position and tween, keep last transformation position, and delete mouseStart (optional)
+        if (this.drag){
+          let interaction = this.determineInteraction(e)
+          this.drag = false
+          let targetDisplacement = this.clamp(interaction.clientX - this.mouseStart.x + this.transformed, this.xBoundary/(2*SCALE), -this.xBoundary/(2*SCALE))
+          this.transformed = targetDisplacement
+          gsap.to(this.$data, {computedDisplacement: targetDisplacement})
+          this.mouseStart.x = undefined
+          this.mouseStart.y = undefined
+        }
+      },
+      clamp(attribute, max, min){
+        return Math.min(max, Math.max(min, attribute))
+      },
+      determineInteraction(e){
+        let interaction = e
+        if(e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel'){
+            let evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent
+            interaction = evt.touches[0] || evt.changedTouches[0]
+        }
+        return interaction
+      },
+      handleResize(){
+        this.xBoundary = document.getElementsByClassName("top-cont")[0].clientWidth
+        if (window.matchMedia("(orientation: landscape)").matches){
+          this.computedDisplacement = 0
+          this.transformed = 0
+        }
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+.main {
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+}
+
+.top-cont {
+  background-color: black;
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding-bottom: 56.25%;
+  @media only screen and (orientation: portrait) {
+    width: 225%;
+    padding-bottom: 126.5625%;
+  }
+}
+
+.canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.slide-controls {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  .arrow {
+    z-index: 99;
+    position: absolute;
+    top: 50%;
+    transform: translate(0, -50%);
+    font-size: 100px;
+    color: rgba($color: white, $alpha: 0.2);
+    transition: color 0.2s ease-in-out;
+    &:hover {
+      color: rgba($color: white, $alpha: 0.8);
+    }
+    &:active {
+      color: rgba($color: white, $alpha: 1.0);
+    }
+    @media only screen and (max-width: 600px) {
+      font-size: 70px;
+    }
+    @media only screen and (max-width: 450px) {
+      font-size: 50px;
+    }
+  }
+  .right-arrow {
+    right: 20px;
+  }
+  .left-arrow {
+    left: 20px;
+  }
+}
+
+.cont {
+  position: absolute;
+  line-height: 0;
+  user-select: none;
+  img {
+    line-height: 0;
+    width: 100%;
+  }
+}
+
+.guide {
+  width: 100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  z-index: 50;
+}
+
+.center-anchor {
+  transform: translate(-50%,-50%);
+}
+
+.tv {
+  width: 25%;
+  top: 37.5%;
+  left: 62.9%;
+}
+.pohon {
+  width: 41.2%;
+  top: -31.7%;
+  left: -3%;
+}
+.karpet {
+  width: 64.5%;
+  top: 56%;
+  left: 35.1%;
+}
+.pagar {
+  width: 100%;
+  top: 31.3%;
+  left: 0;
+}
+.semak1 {
+  width: 31.8%;
+  top: 44.2%;
+  left: 3.6%;
+}
+.semak2 {
+  width: 26.6%;
+  top: 45.2%;
+  left: 37.75%;
+}
+.bunga {
+  width: 37.85%;
+  top: 27.1%;
+  left: 22%;
+}
+
+.sky {
+  width: 100%;
+  top: -37%;
+  left: 0;
+}
+
+.matahari {
+  width: 19.5%;
+  top: -2%;
+  left: 75%;
+}
+
+.pasir {
+  width: 45.5%;
+  top: 57%;
+  left: 54.4%;
+}
+
+.bunga2 {
+  width: 16.45%;
+  top: 45.2%;
+  left: 51.3%;
+}
+
+.tanaman {
+  width: 39%;
+  top: 23.8%;
+  left: 7.8%;
+}
+
+.rumput {
+  width: 100%;
+  top: 52%;
+  left: 0;
+}
+</style>
