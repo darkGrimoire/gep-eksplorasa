449e6c45318a97bd85078110de4e83bffaa69409
Faris Rizki Ekananda <13518125@std.stei.itb.ac.id>
2021-02-23 03:29:38 +0700

    best friend custom slide done

---
+++ pages/karya/_emosi/video/bestfriend.vue
@@ -0,0 +1,396 @@
+<template>
+  <div class="container">
+    <div 
+      v-swiper:mySwiper="swiperOptions"
+      class="swiper"
+    >
+      <div class="swiper-wrapper">
+        <div id="slide1" class="swiper-slide" data-hash="slide1">
+          <div class="slide-container">
+            <zoom-photo v-if="!dbLoading" :poster="dataKarya.posterMin" 
+                        :full="dataKarya.poster" 
+                        :container-class="'poster-container'"
+                        :poster-class="'poster-img'"
+                        @onEnterFull="setButtonLayer(0)"
+                        @onExitFull="setButtonLayer(10)"
+            />
+            <div class="caption-container no-swipe" @mousewheel.stop>
+              <div class="judul">
+                {{ dataKarya.judul }}
+              </div>
+              <div class="author">
+                {{ dataKarya.ph }}
+              </div>
+              <div class="title">
+                {{ dataKarya.captionTitle }}
+              </div>
+              <p class="deskripsi">
+                {{ dataKarya.caption }}
+              </p>
+            </div>
+          </div>
+        </div>
+        <!-- SLIDE 2 -->
+        <div id="slide2" class="swiper-slide narasi" data-hash="slide2">
+          <div class="video-wrapper">
+            <div v-if="videoLoading" class="loading-wrapper">
+              <Loading />
+            </div>
+            <youtube v-if="!dbLoading" ref="youtube" :resize-delay="0" :resize="true" :fit-parent="true" :video-id="dataKarya.videoId[0]" :player-vars="playerVars" @ended="restart" @ready="onPlayerReady" />
+          </div>
+        </div>
+        <!-- TODO: SLIDE 3 -->
+        <!-- Kalo ada deskripsi dan still image lain. Kalo gaada hapus aja -->
+        <div id="slide3" class="swiper-slide narasi" data-hash="slide3">
+          <div class="slide-container">
+            <div class="video-wrapper" style="margin-left: 30px;">
+              <div v-if="videoLoading" class="loading-wrapper">
+                <Loading />
+              </div>
+              <youtube v-if="!dbLoading" ref="youtube" :resize-delay="0" :resize="true" :fit-parent="true" :video-id="dataKarya.videoId[1]" :player-vars="playerVars" @ended="restart" @ready="onPlayerReady" />
+            </div>
+            <div class="caption-container no-swipe" @mousewheel.stop>
+              <div class="judul">
+                {{ dataKarya.judul2 }}
+              </div>
+              <div class="author">
+                {{ dataKarya.ph2 }}
+              </div>
+              <div class="title">
+                {{ dataKarya.captionTitle2 }}
+              </div>
+              <p class="deskripsi">
+                {{ dataKarya.caption2 }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div slot="pagination" class="swiper-pagination" />
+    </div>
+    <nuxt-link class="back-button" :to="'/ruangan/'+emosi">
+      Back
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+import 'swiper/swiper-bundle.css'
+import { Youtube } from 'vue-youtube'
+import ZoomPhoto from '~/components/ZoomPhoto.vue'
+import loading from '~/components/Loading.vue'
+
+  export default {
+    name: 'TemplateVideo',
+    components: {
+      Youtube,
+      ZoomPhoto,
+      loading
+    },
+    async asyncData({ params }) {
+      const emosi = params.emosi
+      return { emosi }
+    },
+    data () {
+      return {
+        dataKarya: {
+          judul: '',
+          ph: '',
+          caption: '',
+          caption2: '',
+          captionTitle: '',
+          videoId: [],
+          posterMin: '',
+          poster: '',
+        }, 
+        swiperOptions: {
+          slidesPerView: 1,
+          speed: 1500,
+          mousewheel: true,
+          centeredSlides: true,
+          direction: 'vertical',
+          keyboard: {
+            enabled: true,
+          },
+          pagination: {
+            clickable: true, 
+            el: '.swiper-pagination',
+            type: 'bullets'
+          },
+          noSwiping: true,
+          noSwipingClass: 'no-swipe'
+        },
+        playerVars: {
+        autoplay: 0,
+        rel: 0
+        },
+        videoLoading: true,
+        dbLoading: true
+      }
+    },
+    computed: {
+      player() {
+        return this.$refs.youtube.player
+      }
+    },
+    mounted () {
+      const karyaRef = this.$fire.firestore.collection('karya').doc('biasa').collection('video').doc('bestfriend')
+      karyaRef.get()
+        .then(doc => {
+          let data = {id: doc.id, ...doc.data()}
+          this.dataKarya.judul = data.judul
+          this.dataKarya.judul2 = data.judul2
+          this.dataKarya.ph = data.ph
+          this.dataKarya.ph2 = data.ph2
+          this.dataKarya.caption = this.handleNewLines(data.caption)
+          this.dataKarya.caption2 = this.handleNewLines(data.caption2)
+          this.dataKarya.captionTitle = data.captionTitle
+          this.dataKarya.captionTitle2 = data.captionTitle2
+          this.dataKarya.videoId = (data.videoId || data.videoid)
+          this.dataKarya.poster = data.poster
+          this.dataKarya.posterMin = 'https://storage.googleapis.com/gep-teknis.appspot.com/karya/video/_bestfriend/minposter-Poster เพื่อนแท้ (Best Friend)(1).png'
+          this.dataKarya.next = data.next
+          this.dataKarya.prev = data.prev
+          this.dbLoading = false
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    methods: {
+      restart(){
+        this.player.cueVideoById(this.dataKarya.videoId)
+      },
+      onPlayerReady(){
+        this.videoLoading = false
+      },
+      handleNewLines(text){
+        return text.replaceAll("$\\n", "\n\n")
+      },
+      setButtonLayer(idx){
+        document.getElementsByClassName('back-button')[0].style.zIndex = idx
+        document.getElementsByClassName('swiper-pagination')[0].style.zIndex = idx
+      },
+      handleMin(text){
+        let pos = text.lastIndexOf('/')+'/'.length
+        return text.slice(0, pos) + 'min-' + text.slice(pos)
+      }
+    },
+    head() {
+      return {
+        title: this.dataKarya.judul,
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            description: 'Video "'+this.dataKarya.judul+'" oleh '+this.dataKarya.ph
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+.container {
+  background-image: url('/img/uc-bg.png');
+  background-size: 100% auto;
+  background-repeat: repeat-y;
+  background-position: center top;
+  height: 100vh;
+  width: 100vw;
+}
+
+.back-button {
+  position: fixed;
+  bottom: 2%;
+  left: 2%;
+  color: white;
+  font-size: 40px;
+  font-family: 'KG Happy Solid';
+  z-index: 1;
+  text-decoration: none;
+  opacity: 0.7;
+  transition: opacity 0.25s ease-in-out;
+  &:hover{
+    cursor: pointer;
+    opacity: 1;
+  }
+  @media only screen and (max-width: 800px) {
+    left: 5%;
+    bottom: 5%;
+    opacity: 1;
+  }
+}
+
+.swiper {
+  height: 100vh;
+  width: 100vw;
+}
+
+.swiper-slide {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  font-size: 38px;
+  font-weight: 700;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.swiper-slide.swiper-slide-active {
+  opacity: 1;
+  transition: opacity 1.5s;
+}
+
+.swiper-pagination {
+  margin-right: 2%;
+}
+
+::v-deep .swiper-pagination-bullet {
+  background-color: rgba($color: white, $alpha: .6);
+  width: 16px;
+  height: 16px;
+}
+
+::v-deep .swiper-pagination-bullet-active {
+  background-color: rgba($color: white, $alpha: .9);
+}
+
+.black {
+  background-color: black;
+}
+
+::v-deep .poster-container {
+    width: 30%;
+    margin-left: 5%;
+    @media only screen and (max-width: 800px) {
+      width: 45%;
+      margin: 2% 0;
+    }
+  }
+
+.slide-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (max-width: 800px) {
+    flex-direction: column;
+  }
+}
+
+.text-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 7;
+}
+
+
+.caption-container {
+  font-family: 'Karla';
+  text-align: left;
+  color: white;
+  margin: 0 5%;
+  max-width: 40%;
+  overflow-y: auto;
+  max-height: 50vh;
+  z-index: 100;
+  .judul {
+    text-align: center;
+    font-size: 48px;
+    font-weight: bold;
+    margin-bottom: 2px;
+  }
+  .author {
+    text-align: center;
+    font-weight: 300;
+    font-style: italic;
+    font-size: 20px;
+    margin-bottom: 25px;
+  }
+  .title {
+    font-size: 35px;
+  }
+  .deskripsi {
+    font-size: 16px;
+    white-space: pre-line;
+  }
+  @media only screen and (max-width: 800px) {
+    max-height: 35vh;
+    min-width: 60vw;
+
+    .title {
+      font-size: 24px;
+    }
+    .deskripsi {
+      font-size: 16px;
+    }
+  }
+  @media only screen and (max-width: 600px) {
+    .judul {
+      font-size: 20px;
+    }
+    .author {
+      font-size: 16px;
+    }
+    .title {
+      font-size: 16px;
+    }
+    .deskripsi {
+      font-size: 12px;
+    }
+  }
+}
+
+.loading-wrapper {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: inherit;
+  height: inherit;
+}
+.video-wrapper {
+  width: 852px;
+  height: 480px;
+  @media only screen and (max-width: 800px) {
+    width: 605px;
+    height: 340px;
+  }
+  @media only screen and (max-width: 600px) {
+    width: 355px;
+    height: 200px;
+  }
+  @media only screen and (max-width: 450px) {
+    width: 285px;
+    height: 160px;
+  }
+}
+
+/* width */
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1; 
+  }
+  
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: #888; 
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555; 
+  }
+</style>
