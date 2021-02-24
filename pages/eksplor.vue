f060bf409da85456ce7118fbc2d90d8a5bb04f43
regnents <rafaelseanputra@gmail.com>
2021-02-24 22:38:48 +0700

    change eksplor to loading page

--- pages/eksplor.vue
+++ pages/eksplor.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="background">
-    <div class="button-container">
-      <nuxt-link class="button" to="/dots" :no-prefetch="true">
-        start exploring
-      </nuxt-link>
-    </div>
+    <div class="button-container">Loading</div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      progress: 0,
+      id: 0
+    }
+  },
   mounted() {
     this.initSetup()
+    let x = setInterval(() => {
+      if (this.progress == 100) {
+        clearInterval(x)
+        this.$router.push({path: '/dots'})
+      } else {
+        this.progress += 10
+        document.getElementsByClassName("button-container")[0].innerHTML =  "Loading " + this.progress + "%"
+      }
+      
+    },500)
   },
   methods: {
     initSetup() {
@@ -19,7 +31,7 @@ export default {
       new Image().src = "/img/dots_2.png"
       new Image().src = "/img/dots_3_flower.png"
       new Image().src = "/img/dots_3_grass.png"
-    }
+    },
   }
 }
 </script>
@@ -39,16 +51,8 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-}
-.button {
-  color: black;
-  all: unset;
   font-family: "Mechanical Pencil";
+  color: black;
   font-size: 40px;
-  z-index: inherit;
-}
-.button:hover {
-  cursor: pointer;
-  text-decoration: underline;
 }
 </style>
