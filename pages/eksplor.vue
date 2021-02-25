ada841739ac8d892302eed128ab3044c6d9d36d4
regnents <rafaelseanputra@gmail.com>
2021-02-26 01:00:04 +0700

    update eksplor

--- pages/eksplor.vue
+++ pages/eksplor.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="background">
-    <div class="button-container">
-      Loading {{ Math.floor(progress) }} %
-    </div>
+    <div class="button-container">Loading {{ Math.floor(progress) }} %</div>
   </div>
 </template>
 
 <script>
-import gsap from 'gsap'
+import gsap from "gsap"
 import Bowser from "bowser"
 export default {
   data() {
@@ -18,16 +16,21 @@ export default {
   },
   mounted() {
     this.initSetup()
-    gsap.to(this.$data, {progress: 100, duration: this.randomInteger(4, 6), ease: 'power3.out', delay: .3,
+    gsap.to(this.$data, {
+      progress: 100,
+      duration: this.randomInteger(4, 6),
+      ease: "power3.out",
+      delay: 0.3,
       onComplete: () => {
         if (browser_name == "Safari") {
-              this.$router.push({ path: "/teras" })
-            } else {
-              this.$router.push({
-                path: "/dots"
-              })
-            }
-      }})
+          this.$router.push({ path: "/teras" })
+        } else {
+          this.$router.push({
+            path: "/dots"
+          })
+        }
+      }
+    })
   },
   methods: {
     initSetup() {
@@ -37,7 +40,7 @@ export default {
       new Image().src = "/img/dots_3_grass.png"
     },
     randomInteger(min, max) {
-      return (Math.random() * (max - min + 1)) + min
+      return Math.random() * (max - min + 1) + min
     }
   }
 }
